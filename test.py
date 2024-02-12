6b075e40c955d18c184fde1866c65322c136d994
Alexandre MATHIAUD <alemat13@gmail.com>
2024-02-12 22:21:25 +0100

    Display localisations in KML Line string format

--- test.py
+++ test.py
@@ -8,3 +8,6 @@ solver = TSPSolver.from_data(localisations[0], localisations[1], norm="GEO")
 solution = solver.solve()
 
 print(csv_data.iloc[solution.tour].to_csv(index=False,sep='\t'))
+
+print("KML Line String")
+[print(x) for x in csv_data.iloc[solution.tour]['Localisation'].str.split(",", expand=True)[[1,0]].apply(lambda x: ",".join(x), axis=1)]
