846a982bbc2d6e3181bfd6f9b1e54f9aac67e003
Alexandre Mathiaud <alemat13@gmail.com>
2024-02-28 13:26:13 +0100

    Refactor GPS coordinates to use arrays and add MarkerInput component

---
+++ frontend/src/components/MarkerInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MarkerInput = ({ marker, onChange }) => {
+    { lat, lon, label } = marker;
+    return (
+        <li>
+            <label>Latitude:</label>
+            <input type="text" value={lat} onChange={onChange} name="lat" />
+            <label>Longitude:</label>
+            <input type="text" value={lon} onChange={onChange} name="long" />
+            <label>Label:</label>
+            <input type="text" value={label} onChange={onChange} name="label" />
+        </li>
+    );
+};
+
+export default MarkerInput;
